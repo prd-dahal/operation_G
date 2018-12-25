63c8b5265725888bbecaf33244d7f7521371224a
unknown <pradeep.dahal222@gmail.com>
2018-12-26 05:10:43 +0545

    addPHP

---
+++ insert.php
@@ -0,0 +1,29 @@
+<?php
+
+$f_number = $_POST['f_number'];
+$full_name = $_POST['full_name'];
+$email = $_POST['email'];
+$education = $_POST['education'];
+$phone_number = $_POST['phone_number'];
+$familyDetails=$_POST['familyDetails'];
+$n_mobile = $_POST['n_mobile'];
+$purpose = $_POST['purpose'];
+$f_purpose = $_POST['f_purpose'];
+$isPC=$_POST['isPC'];
+$Cpurpose=$_POST['Cpurpose']
+$internet = $_POST['internet'];
+$isCS=$_POST['isCS'];
+$programming = $_POST['programming'];
+
+
+$connection = mysqli_connect('localhost', 'root', '', 'operation_G');
+
+$query = "INSERT INTO users(f_number, full_name, email, education, phone_number, n_mobile, purpose, f_purpose, internet, programming) VALUES('$f_number', '$full_name', '$email', '$education', '$phone_number', '$n_mobile', '$purpose', '$f_purpose', '$internet', '$programming')";
+
+$result = mysqli_query($connection, $query);
+
+if(!$result) {
+	die(mysqli_error());
+}
+
+?>
